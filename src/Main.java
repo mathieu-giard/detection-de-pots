a4b6bbd83a7ebf290dba5952cbab1e711e98d45c
Eléonore <eleonore.lavaud@numericable.fr>
2014-03-02 17:12:06 +0100

    dernières modifs

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		
-		
-		
-		
-		
-		
+		MainClass mc = new MainClass();
+		mc.mainAlgo("cercle.png",0,1,1,1,1);
 	}
 
 }
--- src/MainClass.java
+++ src/MainClass.java
@@ -16,19 +16,19 @@ public class MainClass {
 	private Complex[] refCarre;
 	private Complex[] refRond;
 
-	public ArrayList<Rectangle> MainAlgo(String filename,int seuilTinf, int seuilTsup, int seuilS, int seuilL , int seuilTestRondCarre){
+	public ArrayList<Rectangle> mainAlgo(String filename,int seuilTinf, int seuilTsup, int seuilS, int seuilL , int seuilTestRondCarre){
 		decodeimage(filename);
-		 ArrayList<Pixel> s = selec(seuilTinf, seuilTsup, seuilS, seuilL);
-		 ArrayList<ArrayList<Pixel>> t = ComposantesConnexes(s);
-		 ArrayList<ArrayList<Pixel>> u = Contours (t);
-		 ArrayList<ArrayList<Point>> v = signature(u);
-		 ArrayList<Complex[]> w = descripteursDeFourier(v);
-		 ArrayList<Rectangle> ResultatFinal = zonePlante(w,u,seuilTestRondCarre);
+		ArrayList<Pixel> s = selec(seuilTinf, seuilTsup, seuilS, seuilL);
+		ArrayList<ArrayList<Pixel>> t = ComposantesConnexes(s);
+		ArrayList<ArrayList<Pixel>> u = Contours (t);
+		ArrayList<ArrayList<Point>> v = signature(u);
+		ArrayList<Complex[]> w = descripteursDeFourier(v);
+		ArrayList<Rectangle> ResultatFinal = zonePlante(w,u,seuilTestRondCarre);
 		return ResultatFinal;	
 	}
-	
-	
-	
+
+
+
 	public void decodeimage(String filename){	
 		try{
 			//la mÔøΩthode statique ÔøΩread de la classe javax.imageio.ImageIO renvoie
@@ -37,31 +37,31 @@ public class MainClass {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		}
+	}
 
 	public ArrayList<Pixel> selec(int seuilTinf, int seuilTsup, int seuilS, int seuilL){
 		//selection des bons pixels
 		ArrayList<Pixel> Choisi = new ArrayList<Pixel>();
-		
+
 		for(int x = 0; x < img.getWidth(); x++){
-        	for(int y = 0; y < img.getHeight(); y++){//parcourir l'image
-        	
-        		Color c = new Color(img.getRGB(x, y));//prendre des valeurs RGB de chaque pixel
-            	int r = c.getRed();
-            	int g = c.getGreen();
-            	int b = c.getBlue();
-            	
-            	float[] hsb = Color.RGBtoHSB(r, g, b, null); //convertir RGB en HSB
-		
-            	if ( hsb[1]> seuilS ){
-            		if (seuilTinf < hsb[0] && hsb[0]< seuilTsup){
-            			Pixel P = new Pixel(x,y,hsb[0],hsb[1],hsb[2]);
-            			Choisi.add(P);
-            		}
-            	}
-        	}
+			for(int y = 0; y < img.getHeight(); y++){//parcourir l'image
+
+				Color c = new Color(img.getRGB(x, y));//prendre des valeurs RGB de chaque pixel
+				int r = c.getRed();
+				int g = c.getGreen();
+				int b = c.getBlue();
+
+				float[] hsb = Color.RGBtoHSB(r, g, b, null); //convertir RGB en HSB
+
+				if ( hsb[1]> seuilS ){
+					if (seuilTinf < hsb[0] && hsb[0]< seuilTsup){
+						Pixel P = new Pixel(x,y,hsb[0],hsb[1],hsb[2]);
+						Choisi.add(P);
+					}
+				}
+			}
 		}	
-            	
+
 		return Choisi;
 	}
 
@@ -69,7 +69,7 @@ public class MainClass {
 		int i = 0;
 		int L = Integer.MAX_VALUE;
 		ArrayList<ArrayList<Pixel>> CC = new ArrayList<ArrayList<Pixel>>();
-		
+
 		for(Pixel pixel1 : choisi){
 			int a = pixel1.pixelVoisinGauche(img).getNumeroPixel();
 			int b = pixel1.pixelVoisinHaut(img).getNumeroPixel();
@@ -80,17 +80,17 @@ public class MainClass {
 				pixel1.setNumeroPixel(a);
 			} else {
 				pixel1.setNumeroPixel(Math.min(a,b));
-				
+
 				for(Pixel pixel2 : choisi){
 					int c = pixel2.getNumeroPixel();
-					
+
 					if (c == Math.max(a, b) && Math.max(a, b)!= L){
 						pixel2.setNumeroPixel(Math.min(a,b)) ;
 					}	
 				}
 			}
 		}
-		
+
 		for(int k=0; k<i ; k++){
 			ArrayList<Pixel> cc = new ArrayList<Pixel>();
 			for(Pixel pixel3 : choisi){
@@ -102,10 +102,10 @@ public class MainClass {
 				CC.add(cc);
 			}
 		}
-		
+
 		return CC;
 	}
-	
+
 	public boolean BelongsTocc(Pixel pixel, ArrayList<Pixel> cc){
 		boolean b= false;
 		for (Pixel p: cc){
@@ -115,42 +115,42 @@ public class MainClass {
 		}
 		return b;
 	}
-	
+
 	public ArrayList<ArrayList<Pixel>> Contours(ArrayList<ArrayList<Pixel>> CC){
 		ArrayList<ArrayList< Pixel> > CONTOURS = new ArrayList<ArrayList<Pixel>>();
 		ArrayList<Pixel> contours= new ArrayList<Pixel>();
 		for (ArrayList<Pixel> aa: CC){
 			for(Pixel P: aa){
-	        		if (BelongsTocc(P, aa)==false){
-	        			if (BelongsTocc(P.pixelVoisinDroite(img), aa)==true || BelongsTocc(P.pixelVoisinGauche(img), aa)==true || BelongsTocc(P.pixelVoisinHaut(img), aa)==true || BelongsTocc(P.pixelVoisinBas(img), aa)==true){
-	        				contours.add(P);
-	        			}
-	        		}
-	        }
-		CONTOURS.add(contours);	
+				if (BelongsTocc(P, aa)==false){
+					if (BelongsTocc(P.pixelVoisinDroite(img), aa)==true || BelongsTocc(P.pixelVoisinGauche(img), aa)==true || BelongsTocc(P.pixelVoisinHaut(img), aa)==true || BelongsTocc(P.pixelVoisinBas(img), aa)==true){
+						contours.add(P);
+					}
+				}
+			}
+			CONTOURS.add(contours);	
 		}
 		return CONTOURS;
 	}
-	
-	
+
+
 	public ArrayList<ArrayList<Point>> signature(ArrayList<ArrayList<Pixel>> CC){
-		
+
 		int Sx = 0;
 		int Sy = 0;
 		ArrayList<Point> courbe = new ArrayList<Point>();
 		ArrayList<ArrayList<Point>> COURBE = new ArrayList<ArrayList<Point>>();
 		for (ArrayList<Pixel> cc : CC){
-			
+
 			for (Pixel pixel: cc){
 				Sx=Sx+pixel.getX();
 				Sy=Sy+pixel.getY();
 			}
-			
+
 			Sx=(int) Sx/ cc.size();
 			Sy=(int) Sy/ cc.size();
 			//Pixel G = new Pixel( Sx, Sy,img);
 			Point P;
-			
+
 			for (Pixel p: cc){
 				float r= (float) Math.sqrt((p.getX()- Sx)^2 + (p.getY()- Sy)^2);
 				float O= (float) Math.atan((p.getY()-Sy)/(p.getX()-Sx));
@@ -161,7 +161,7 @@ public class MainClass {
 		COURBE.add(courbe);
 		return COURBE;
 	}
-	
+
 	/*public void paint(ArrayList<ArrayList<Point>> COURBE, Graphics g){
 		for (ArrayList<Point> C: COURBE){
 			for (int i=0 ; i< C.size()-1; i++){
@@ -170,12 +170,12 @@ public class MainClass {
 				double y= C.get(i).getY();
 				double yy = C.get(i+1).getY();
 				g.drawLine( x,(int) y,xx,(int) yy);
-				
+
 			}
 		}
 	}
-	*/
-	
+	 */
+
 	public ArrayList<Complex[]> descripteursDeFourier(ArrayList<ArrayList<Point>> COURBE){
 		FastFourierTransformer math = new FastFourierTransformer(DftNormalization.UNITARY);
 		//XYSeries serie = this.audioData.getSeries(0);
@@ -190,25 +190,25 @@ public class MainClass {
 				serie[j][1]= courbe.get(j).getY();
 			}
 			int k = (int) Math.ceil(Math.log(serie.length)/Math.log(2));
-			
+
 			double[] data = new double[(int) Math.pow(2, k)];
 			Complex[] cResult = math.transform(data, TransformType.FORWARD);
 			Coef.add(cResult);
 		}
 		return Coef ;
-			
+
 	}
-/* ********** remarque:
+	/* ********** remarque:
   il nous faut une image test dont on est sur qu'elle 
   est un carré ou un cercle ce qui nous donne les coef de fourier des signatures de référence
   on pourra ainsi comparer les descripteurs obtenus avec ceux de ref
-  
+
    il manque une méthode comparaison des descripteurs:
    planteRepéréeParLeCarré qui prend en entrée les descrip l'image l'arrayList<ArrayList<Pixel>>
    et qui renvoit une zone au dessus du stikers carré (éventuellement nulle)
-  
- */
-	
+
+	 */
+
 	public boolean comparaisonDescripteursCarre(Complex[] test, float seuil){
 		boolean estCarre = false; 
 		double diff=0;
@@ -220,7 +220,7 @@ public class MainClass {
 		}
 		return estCarre;
 	}
-	
+
 	public boolean comparaisonDescripteursRond(Complex[] test, float seuil){
 		boolean estRond = false; 
 		double diff=0;
@@ -232,17 +232,17 @@ public class MainClass {
 		}
 		return estRond;
 	}
-	
+
 	public ArrayList<Rectangle> zonePlante(ArrayList<Complex[]> DF, ArrayList<ArrayList<Pixel>> contours, float seuil){
 		ArrayList<Rectangle> R= new ArrayList<Rectangle>();
 		for (int i = 0; i< DF.size();i++){
-			
+
 			if(comparaisonDescripteursCarre(DF.get(i),seuil)){
 				int xmax=-1;
 				int xmin=Integer.MAX_VALUE;
 				int ymax =0;
 				String carre= new String();
-				
+
 				for( Pixel p :contours.get(i)){
 					int x=p.getX();
 					int y=p.getY();
@@ -255,7 +255,7 @@ public class MainClass {
 					if(ymax<y){
 						ymax=y;
 					}
-					
+
 				}
 				Point p1 = new Point(xmin,0);
 				Point p2 = new Point(xmin,ymax);
@@ -269,7 +269,7 @@ public class MainClass {
 				int xmin=Integer.MAX_VALUE;
 				int ymax =0;
 				String rond= new String();
-				
+
 				for( Pixel p :contours.get(i)){
 					int x=p.getX();
 					int y=p.getY();
@@ -282,7 +282,7 @@ public class MainClass {
 					if(ymax<y){
 						ymax=y;
 					}
-					
+
 				}
 				Point p1 = new Point(xmin,0);
 				Point p2 = new Point(xmin,ymax);
@@ -290,17 +290,17 @@ public class MainClass {
 				Point p4 = new Point(xmax,ymax);
 				Rectangle r = new Rectangle(p1,p2,p3,p4,rond );
 				R.add(r);
-				
+
 			}
 		}
 		return R;
 	}
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
 }
-	
+
